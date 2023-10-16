44c35a26e09ce810d3b277b7da33b186b0b525c5
Sharon Arana <xel.dev3@inxel.mx>
2023-10-16 11:01:16 -0600

    Update auth with JWT validation

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,34 +1,44 @@
 const { findByUsername } = require("../services/user");
 const jwt = require("jsonwebtoken");
+const { validarTokenJWT } = require("./jwt");
 
 exports.login = async function (request, response) {
-	const { username, password } = request.body;
+  const { username, password } = request.body;
 
-	const user = await findByUsername({ username });
-	console.log(user)
+  const user = await findByUsername({ username });
 
-	if (!user) {
-		return response.status(400).json({
-			message: "Usuario o contraseña inválidos",
-			messagedev: "No se encontro el usuario en la base de datos",
-			code: "ERR_AUTH",
-		});
-	}
+  if (!user) {
+    return response.status(400).json({
+      message: "Usuario o contraseña inválidos",
+      messagedev: "No se encontró el usuario en la base de datos",
+      code: "ERR_AUTH",
+    });
+  }
 
-	if (user.password !== password) {
-		return response.status(400).json({
-			message: "Usuario o contraseña inválidos",
-			messagedev: "No se encontro el usuario en la base de datos",
-			code: "ERR_AUTH",
-		});
-	}
+  if (user.password !== password) {
+    return response.status(400).json({
+      message: "Usuario o contraseña inválidos",
+      messagedev: "Contraseña incorrecta",
+      code: "ERR_AUTH",
+    });
+  }
 
-	const token = jwt.sign(
-		{ id: user.id, username: user.username },
-		process.env.JWT_SECRET
-	);
+  const token = jwt.sign(
+    { id: user.id, username: user.username },
+    process.env.JWT_SECRET
+  );
 
-	response.status(200).json({
-		jwt: token,
-	});
+  const resultadoValidacion = validarTokenJWT({ jwt: token });
+
+  if (resultadoValidacion.error) {
+    return response.status(500).json({
+      message: "Error interno del servidor",
+      messagedev: "Error al generar y validar el token",
+      code: "ERR_INTERNAL",
+    });
+  }
+
+  response.status(200).json({
+    jwt: token,
+  });
 };
