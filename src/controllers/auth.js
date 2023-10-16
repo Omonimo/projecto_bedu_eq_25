c9b32b14c4c56595d879e76ba1ae57035da03058
Sharon Arana <xel.dev3@inxel.mx>
2023-10-16 11:09:14 -0600

    Update auth

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,16 +1,16 @@
 const { findByUsername } = require("../services/user");
 const jwt = require("jsonwebtoken");
-const { validarTokenJWT } = require("./jwt");
 
 exports.login = async function (request, response) {
   const { username, password } = request.body;
 
   const user = await findByUsername({ username });
+  console.log(user);
 
   if (!user) {
     return response.status(400).json({
       message: "Usuario o contraseña inválidos",
-      messagedev: "No se encontró el usuario en la base de datos",
+      messagedev: "No se encontro el usuario en la base de datos",
       code: "ERR_AUTH",
     });
   }
@@ -18,7 +18,7 @@ exports.login = async function (request, response) {
   if (user.password !== password) {
     return response.status(400).json({
       message: "Usuario o contraseña inválidos",
-      messagedev: "Contraseña incorrecta",
+      messagedev: "No se encontro el usuario en la base de datos",
       code: "ERR_AUTH",
     });
   }
@@ -27,16 +27,6 @@ exports.login = async function (request, response) {
     { id: user.id, username: user.username },
     process.env.JWT_SECRET
   );
-
-  const resultadoValidacion = validarTokenJWT({ jwt: token });
-
-  if (resultadoValidacion.error) {
-    return response.status(500).json({
-      message: "Error interno del servidor",
-      messagedev: "Error al generar y validar el token",
-      code: "ERR_INTERNAL",
-    });
-  }
 
   response.status(200).json({
     jwt: token,
